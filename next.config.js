a551e20113bb3571cba2879a958d97f0bf357545
Portail SPI-CAM <zembesseval@gmail.com>
2026-06-06 03:55:48 +0200

    fix: add webpack alias for @/ resolution on Render

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, './'),
+    };
+    return config;
   },
 };
 
