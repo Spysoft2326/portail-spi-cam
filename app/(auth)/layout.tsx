99c34393adafd40e02bec7f6338cd00a68017c76
Portail SPI-CAM <zembesseval@gmail.com>
2026-06-06 04:51:13 +0200

    fix: pass user prop to Sidebar component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
-﻿import { authOptions } from "../../lib/auth";
+﻿import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../lib/auth";
 import { Sidebar } from "@/components/ui/sidebar";
 import Header from "@/components/ui/header";
 
@@ -7,11 +8,13 @@ export default async function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex flex-1">
-        <Sidebar />
+        <Sidebar user={session?.user} />
         <main className="flex-1 p-6">
           {children}
         </main>
