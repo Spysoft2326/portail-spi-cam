04154432fdd47c67331322a9207289cf6ffc524e
Portail SPI-CAM <zembesseval@gmail.com>
2026-06-08 19:34:05 +0200

    fix: add build.sh for Render

---
+++ build.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+npm install
+rm -rf node_modules/.prisma
+rm -rf node_modules/@prisma/client
+npx prisma generate --schema=prisma/schema.prisma
+npx prisma db push
+node prisma/seed-enterprises-150.js
+node prisma/seed-users.js
+npm run build
