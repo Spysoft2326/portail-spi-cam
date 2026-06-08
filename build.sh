05ae782bb10cd5823ae8d88ae715de6cd994c937
Portail SPI-CAM <zembesseval@gmail.com>
2026-06-08 19:47:30 +0200

    fix: schema.prisma with Entreprise capital E

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 ﻿#!/bin/bash
 npm install
+
+# Vérifier et corriger le schéma Prisma si nécessaire
+if ! grep -q "model Entreprise" prisma/schema.prisma; then
+  echo "Corriger le schéma Prisma..."
+  sed -i 's/model entreprise/model Entreprise/g' prisma/schema.prisma
+  sed -i 's/entreprise    Enterprise/Entreprise    Entreprise/g' prisma/schema.prisma
+fi
+
 rm -rf node_modules/.prisma
 rm -rf node_modules/@prisma/client
 npx prisma generate --schema=prisma/schema.prisma
