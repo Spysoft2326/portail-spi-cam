4e1dffd9f9a86bd4a3ed8cf8db2c7f94431d821f
Portail SPI-CAM <zembesseval@gmail.com>
2026-06-11 00:33:50 +0200

    cleanup: remove backup and temp files

--- app/dashboard/entreprises/Paold.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import EntreprisesContent from "@/components/entreprises/entreprises-content";
-
-export default function DashboardEntreprisesPage() {
-  return <EntreprisesContent />;
-}
--- app/dashboard/parametres/users/paold.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ParametresContent from "@/components/parametres/parametres-content";
-
-export default function DashboardParametresPage() {
-  return <ParametresContent />;
-}
